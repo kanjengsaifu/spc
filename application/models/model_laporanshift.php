b9d1ff485d618d62f410c94a46acb1d9deebbbc9
majid <majid_spell2@yahoo.co.id>
2015-04-04 09:37:22 +0700

    pagination search model

--- application/models/model_laporanshift.php
+++ application/models/model_laporanshift.php
@@ -1,16 +1,34 @@
 <?php
-class model_laporanshift extends CI_Model{
-    function getlaporanshift(){
+
+class model_laporanshift extends CI_Model {
+
+    function getlaporanshift() {
         return $this->db->get('laporanshift');
     }
-    
-    function deletelaporanshift($id){
+
+    function getlaporanshiftpaging($halaman, $batas) {
+        $query = "SELECT * FROM laporanshift LIMIT $halaman,$batas";
+        return $this->db->query($query);
+    }
+
+    function getlaporanshiftbyid($id) {
+        $param = array('idlaporanshift' => $id);
+        return $this->db->get_where('laporanshift', $param);
+    }
+
+    function deletelaporanshift($id) {
         $this->db->where('idlaporanshift', $id);
         $this->db->delete('laporanshift');
     }
-    
-    function laporanbydate(){
-        $query="";
+
+    function getlaporanshiftbydate($tgl1, $tgl2) {
+        $query = "SELECT * FROM laporanshift WHERE tgllaporanshift BETWEEN '$tgl1' and '$tgl2'";
         return $this->db->query($query);
     }
+
+    function getlaporanshiftbydatepaging($tgl1, $tgl2, $halaman, $batas) {
+        $query = "SELECT * FROM laporanshift WHERE tgllaporanshift BETWEEN '$tgl1' and '$tgl2' LIMIT $halaman,$batas";
+        return $this->db->query($query);
+    }
+
 }
